70bab3b082ff3497bc2e87d54116cc06193d5228
d1lmurodvic <dildoraishmuhamedova4@gmail.com>
2025-06-21 15:28:02 +0500

    Asdsda

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -6,21 +6,20 @@ import DiedPeople from './DiedPeople';
 
 const Game = () => {
 
-    
     return (
         <div className='flex h-screen '>
-            <div className='w-1/3 flex justify-center h-full bg-success-content '>
+            <div className='w-1/3 flex justify-center h-full'>
                 <GameCard/>
             </div>
-            <div className='w-2/3 h-full bg-green-400 flex flex-col'>
-                <div className='h-1/3 bg-amber-500'>
+            <div className='w-2/3 h-full flex flex-col'>
+                <div className='h-1/3'>
                     <p>Players</p>
                 </div>
                 <div className='h-2/3 w-full justify-center flex-1'>
                     <GameChat/>
                 </div>
             </div>
-            <div className='w-1/4 h-full bg-blue-400'>
+            <div className='w-1/4 h-full'>
                 <DiedPeople/>
             </div>
         </div>
